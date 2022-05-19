586861a719b678cb1f86b2c80bfe9aa2793fbd33
SangBeom-Lee <bumeeya2@naver.com>
2022-05-19 23:12:20 +0900

    template 적용

---
+++ layout/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+# Create your views here.
+class MainView(ListView):
+
+    template_name                       = 'layout/index.html'
+    context_object_name                 = 'data'
+
+    def get_queryset(self):
+        return ''
+
+    def get_context_data(self, **kwargs):
+        context                         = super().get_context_data(**kwargs)
+        context['css_file']             = 'inc/css/index.css'
+        context['js_file']              = 'inc/js/index.js'
+        return context
