c501ab6bba384debd8b00b26b9896ee20f4a5690
SangBeom-Lee <bumeeya2@naver.com>
2022-05-31 22:29:51 +0900

    gitignore추가 및 기본 css파일추가 및 서브페이지 추가

--- layout/views.py
+++ layout/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-# Create your views here.
+# Main
 class MainView(ListView):
 
     template_name                       = 'layout/main.html'
@@ -15,3 +15,18 @@ class MainView(ListView):
         context['css_file']             = 'inc/css/index.css'
         context['js_file']              = 'inc/js/index.js'
         return context
+
+
+# Intro (회사소개)
+class IntroView(ListView):
+
+    template_name                       = 'subpage/intro.html'
+    context_object_name                 = 'data'
+
+    def get_queryset(self):
+        return ''
+
+    def get_context_data(self, **kwargs):
+        context                         = super().get_context_data(**kwargs)
+        return context
+        
